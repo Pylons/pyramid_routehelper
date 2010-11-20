3cd968c07b51d0ad54a5fa86e3d07f6ab5a70288
Ben Bangert <ben@groovie.org>
2010-11-20 12:43:17 -0800

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,23 @@
+Route Helpers for Pyramid
+=========================
+
+``pyramid_routehelper`` is a package of RPC related add-on'
+
+Support and Documentation
+-------------------------
+
+See the `pyramid_routehelper website
+<http://docs.pylonshq.com/pyramid_routehelper/dev/>`_ to view documentation,
+report bugs, and obtain support.
+
+License
+-------
+
+``pyramid_routehelper`` is offered under the BSD-derived `Repoze Public
+License <http://repoze.org/license.html>`_.
+
+Authors
+-------
+
+``pyramid_routehelper`` is made available by `Agendaless Consulting
+<http://agendaless.com>`_ and a team of contributors.
---
+++ setup.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+# Copyright (c) 2008-2010 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+__version__ = '0.1'
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+setup(name='pyramid_routehelper',
+      version=__version__,
+      description='Route Helpers for Pyramid',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Framework :: Pylons",
+        ],
+      keywords='web wsgi pyramid pylons',
+      author="Ben Bangert",
+      author_email="ben@groovie.org",
+      url="http://pylonshq.com/",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require = ['pyramid', 'pkginfo'],
+      install_requires=['setuptools','pyramid'],
+      test_suite="pyramid_routehelper",
+      )
